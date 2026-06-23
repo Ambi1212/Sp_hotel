f73b450a2701048e58a0a14bef941bc65993ee58
Ambi1212 <ambikakm19120@gmail.com>
2026-06-23 15:07:38 +0530

    code added

---
+++ first.java
@@ -0,0 +1,13 @@
+ public class demo{
+    int id ;
+    String name;
+    String address ;
+
+    demo(){
+        System.out.println("empty param");
+    }
+
+ }
+ public static void mail (String[]args){
+    demo d= new demo();
+ }
